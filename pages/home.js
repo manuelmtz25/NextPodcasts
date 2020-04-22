97663693d4327bccfc0270427d9ce7a153055c07
Manuel Martínez <manuelmtz_25@hotmail.com>
2020-04-22 08:42:05 -0500

    Component like NextJs documentation

---
+++ pages/home.js
@@ -0,0 +1,16 @@
+function Home() {
+    return (
+        <>
+            <h1>Welcome to Next.js!</h1>
+            <h2>Like NextJs documentation</h2>
+
+            <style jsx>{`
+                h1 {
+                    color: green;
+                }
+            `}</style>
+        </>
+    );
+}
+
+export default Home;
