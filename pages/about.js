7f9fe9808de6c047143c83c44ec4505a4c6c8b08
Manuel Martínez <manuelmtz_25@hotmail.com>
2020-04-22 08:31:36 -0500

    .prettierrc + React.Fragment

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,16 @@
-export default class extends React.Component{
-    render(){
-        return  <div>
-                    <h1>Manuel Martínez Alvarez</h1>
-                    <h2>Curso de Platzi</h2>
-                </div>
+export default class extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>Manuel Martínez Alvarez</h1>
+                <h2>Curso de Platzi</h2>
+
+                <style jsx>{`
+                    h1 {
+                        color: blue;
+                    }
+                `}</style>
+            </>
+        );
     }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,34 @@
-export default class extends React.Component{
-    render(){
-        return  <div>
-                    <h1>¡Hola MMA!</h1>
-                    <p>Bienvenidos al curso de Next.js</p>
+export default class extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>¡Hola MMA!</h1>
+                <p>Bienvenidos al curso de Next.js</p>
 
-                    <img src="/static/logo.png" alt="Logo" />
+                <img src="/static/logo.png" alt="Logo" />
 
-                    <style jsx>{`
-                        h1{
-                            color: red;
-                        }
+                <style jsx>{`
+                    h1 {
+                        color: red;
+                    }
 
-                        p{
-                            color: green;
-                        }
+                    p {
+                        color: green;
+                    }
 
-                        img{
-                            max-width:50%;
-                            display:block;
-                            margin: 0 auto;
-                        }
-                    `}</style>
+                    img {
+                        max-width: 50%;
+                        display: block;
+                        margin: 0 auto;
+                    }
+                `}</style>
 
-                    <style jsx global>{`
-                        body{
-                            background: white;
-                        }
-                    `}</style>
-                </div>
+                <style jsx global>{`
+                    body {
+                        background: white;
+                    }
+                `}</style>
+            </>
+        );
     }
 }
